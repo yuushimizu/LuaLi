94820e9ad075d97931913f8c53648df66ff90102
Yuu Shimizu <yuu.0125@gmail.com>
2016-10-19 03:39:20 +0900

    refactor

--- parse.lua
+++ parse.lua
@@ -30,16 +30,20 @@ function session:spend(length)
   self.buffer = self.buffer:sub(length + 1)
 end
 
+function session:push_back(s)
+  self.buffer = s .. self.buffer
+end
+
 function session:emit(form)
   coroutine.yield(function() self.callback(form) end)
 end
 
-function session:next_match(pattern, keep_last)
+function session:next_match(pattern)
   while true do
     local s = self.buffer
     local match_start, match_end = s:find(pattern)
     if match_start and (self.eof or match_end < s:len()) then
-      self:spend(match_end - (keep_last and 1 or 0))
+      self:spend(match_end)
       return s:match(pattern)
     end
     if self.eof then
@@ -58,20 +62,16 @@ function session:parse_symbol()
   return form.symbol(token)
 end
 
-function session:next_token_start()
-  return self:next_match("%S", true)
-end
-
 function session:parse_next(additional_special_characters)
-  local token_start = self:next_token_start()
+  local token_start = self:next_match("%S")
   if not token_start then
     return
   end
   local special_parser = (additional_special_characters and additional_special_characters[token_start]) or special_characters[token_start]
   if special_parser then
-    self:spend(1)
     return special_parser(self)
   end
+  self:push_back(token_start)
   return self:parse_symbol()
 end
 
